7d1bd4275ea8c4f19a2bb4dbe6290fa42e2997a4
Maciek <matomaciek@gmail.com>
2024-08-22 19:02:50 +0200

    plan

---
+++ src/notatki/aplikacja-plan.ts
@@ -0,0 +1,23 @@
+/*
+1. Dodac filtr dla terpenow
+2. Zrobic ProductPreview (obsluzyc dane, ktore pobieramy i wyswietlic je w przystepny dla uzytkownika sposob)
+3. Zrobic rejestracje
+    - Zrobic nowa podstrone Register
+    - Zrobic formularz rejestracji (z walidacja: email, haslo, powtorz haslo)
+    - Zrobic nowy endpoint na backendzie do rejestracji ( z zapisem uzytkownika do bazy)
+    - Spiac front i backend
+    - Zrobic szyfrowanie hasla na backendzie*
+4. Zrobic logowanie
+    - Zrobic nowa podstrone Login
+    - Zrobic formularz logowania (z walidacja)
+    - Zrobic nowy endpoint na backendzie do logowania (sprawdzanie credentials)
+    - Zrobic autentykacje JWT*
+5. Zrobic ProtectedRoutes, czyli podstrony tylko dla zalogowanych uzytkownikow
+*/
+
+/*
+Plan dzialania:
+1. Na kazde zadanie tworzymy oddzielny branch gita (git checkout -b <moja_nazwa_brancha> np: create-register)
+2. Po zrobieniu kazdego zadania robimy pull request
+3. Jak mamy approve to robimy merge
+*/
