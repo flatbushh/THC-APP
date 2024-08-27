67492a25326ddb48ff391fe53b072d665ed1aa26
Maciek <matomaciek@gmail.com>
2024-08-27 18:18:40 +0200

    test commit

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,77 +1,81 @@
 /* eslint-disable no-nested-ternary */
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import InventoryIcon from '@mui/icons-material/Inventory';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import PercentIcon from '@mui/icons-material/Percent';
-import BoltIcon from '@mui/icons-material/Bolt';
-import GrassIcon from '@mui/icons-material/Grass';
-import { Filter2Sharp, Filter2TwoTone } from '@mui/icons-material';
-import Checkbox from '@mui/material/Checkbox';
-import { useState } from 'react';
-import Slider from '@mui/material/Slider';
-import TextField from '@mui/material/TextField';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { FilterTypeKeys, FiltersType, Product } from '../Pages/ProductsList';
-import { TerpenEnum } from '../types/Terpen';
-import { switchTerpenIcon } from '../utils/switchTerpenIcon';
-import { GeneticsEnum } from '../types/GeneticsEnum';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import PercentIcon from "@mui/icons-material/Percent";
+import BoltIcon from "@mui/icons-material/Bolt";
+import GrassIcon from "@mui/icons-material/Grass";
+import { Filter2Sharp, Filter2TwoTone } from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
+import { useState } from "react";
+import Slider from "@mui/material/Slider";
+import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { FilterTypeKeys, FiltersType, Product } from "../Pages/ProductsList";
+import { TerpenEnum } from "../types/Terpen";
+import { switchTerpenIcon } from "../utils/switchTerpenIcon";
+import { GeneticsEnum } from "../types/GeneticsEnum";
 
 const drawerWidth = 240;
 
-const FILTER_NAMES = [TerpenEnum.CARIOPHILEN, TerpenEnum.LIMONEN, TerpenEnum.MIRCEN];
+const FILTER_NAMES = [
+  TerpenEnum.CARIOPHILEN,
+  TerpenEnum.LIMONEN,
+  TerpenEnum.MIRCEN,
+];
 
 const marks = [
   {
     value: 0,
-    label: '0%',
+    label: "0%",
   },
   {
     value: 10,
-    label: '10%',
+    label: "10%",
   },
   {
     value: 20,
-    label: '20%',
+    label: "20%",
   },
   {
     value: 30,
-    label: '30%',
+    label: "30%",
   },
   {
     value: 40,
-    label: '40%',
+    label: "40%",
   },
 ];
 function valuetext(value: number) {
   return `${value}%`;
 }
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
 interface AppBarProps extends MuiAppBarProps {
@@ -79,16 +83,16 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -96,15 +100,25 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 type ProductDrawerType = {
-    open: boolean,
-    handleDrawerOpen:() => void,
-    handleDrawerClose: () => void,
-    selectedGenetics: string | null
-    filterElements: (key: FilterTypeKeys, value: number | number[] | GeneticsEnum | string) => void
-}
+  open: boolean;
+  handleDrawerOpen: () => void;
+  handleDrawerClose: () => void;
+  selectedGenetics: string | null;
+  selectedTerpen: string | null;
+  filterElements: (
+    key: FilterTypeKeys,
+    value: number | number[] | GeneticsEnum | string
+  ) => void;
+};
 
-export const ProductDrawer:React.FC<ProductDrawerType> =
-({ filterElements, selectedGenetics, open, handleDrawerOpen, handleDrawerClose }) => {
+export const ProductDrawer: React.FC<ProductDrawerType> = ({
+  filterElements,
+  selectedGenetics,
+  selectedTerpen,
+  open,
+  handleDrawerOpen,
+  handleDrawerClose,
+}) => {
   const theme = useTheme();
 
   // const filterProducts = (terpen: TerpenEnum) => {
@@ -120,7 +134,7 @@ export const ProductDrawer:React.FC<ProductDrawerType> =
   // };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <AppBar position="fixed" open={open}>
         <Toolbar>
           <IconButton
@@ -128,7 +142,7 @@ export const ProductDrawer:React.FC<ProductDrawerType> =
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
@@ -142,9 +156,9 @@ export const ProductDrawer:React.FC<ProductDrawerType> =
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
+            boxSizing: "border-box",
           },
         }}
         variant="persistent"
@@ -153,13 +167,17 @@ export const ProductDrawer:React.FC<ProductDrawerType> =
       >
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </DrawerHeader>
 
         <Divider />
         <List>
-          {FILTER_NAMES.map((filter, index) =>
+          {FILTER_NAMES.map((filter, index) => (
             <ListItem key={index}>
               <ListItemIcon>{switchTerpenIcon(filter)}</ListItemIcon>
               <ListItemText>{filter}</ListItemText>
@@ -168,67 +186,117 @@ export const ProductDrawer:React.FC<ProductDrawerType> =
               jego wbudowane propsy, wywolujemy nasza funkcje filterProducts na onChange i podajemy do niego nasz filter
               ktory jest tak na prawde nazwa terpenu jako argument */}
               {/* <Checkbox onChange={() => filterProducts(filter)}/> */}
+              <ListItem>
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        onChange={(event) =>
+                          filterElements(
+                            "terpen",
+                            event.target.value as TerpenEnum
+                          )
+                        }
+                      />
+                    }
+                    label="terpen"
+                    value={TerpenEnum}
+                    checked={selectedTerpen === TerpenEnum.CARIOPHILEN}
+                  />
+                </FormGroup>
+              </ListItem>
             </ListItem>
-          )}
+          ))}
         </List>
 
-        <List className='producent'>
-
+        <List className="producent">
           <ListItem>
-            <Box
-              component="form"
-              noValidate
-              autoComplete="off"
-            >
-              <TextField id="outlined-basic" label="Producent" variant="outlined" onChange={(event) => filterElements('producentName', event.target.value)} />
-
-            </Box>
-          </ListItem>
-        </List>
-
-        <List className='genetyka'>
-          <ListItem>
-            <FormGroup>
-              <FormControlLabel  control={<Checkbox onChange={(event) => filterElements('genetics', event.target.value as GeneticsEnum)}  />} label="Indica" value={GeneticsEnum.INDICA} checked={selectedGenetics === GeneticsEnum.INDICA}/>
-              <FormControlLabel control={<Checkbox onChange={(event) => filterElements('genetics', event.target.value as GeneticsEnum)}  />} label="Sativa" value={GeneticsEnum.SATIVA} checked={selectedGenetics === GeneticsEnum.SATIVA}/>
-
-            </FormGroup>
-          </ListItem>
-        </List>
-
-        <List className='thc level'>
-          <ListItem>
-            <Box sx={{ width: 300 }}>
-              <Slider min={0} max={40}
-                getAriaLabel={() => 'thcLevel'}
-                defaultValue={[0, 40]}
-                getAriaValueText={valuetext}
-                step={5}
-                valueLabelDisplay="auto"
-                marks={marks}
-                onChange={(event, value) => filterElements('thcLevel',value)}
+            <Box component="form" noValidate autoComplete="off">
+              <TextField
+                id="outlined-basic"
+                label="Producent"
+                variant="outlined"
+                onChange={(event) =>
+                  filterElements("producentName", event.target.value)
+                }
               />
             </Box>
           </ListItem>
         </List>
 
-        <List className='cbd level'>
+        <List className="genetyka">
+          <ListItem>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={(event) =>
+                      filterElements(
+                        "genetics",
+                        event.target.value as GeneticsEnum
+                      )
+                    }
+                  />
+                }
+                label="Indica"
+                value={GeneticsEnum.INDICA}
+                checked={selectedGenetics === GeneticsEnum.INDICA}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={(event) =>
+                      filterElements(
+                        "genetics",
+                        event.target.value as GeneticsEnum
+                      )
+                    }
+                  />
+                }
+                label="Sativa"
+                value={GeneticsEnum.SATIVA}
+                checked={selectedGenetics === GeneticsEnum.SATIVA}
+              />
+            </FormGroup>
+          </ListItem>
+        </List>
+
+        <List className="thc level">
           <ListItem>
             <Box sx={{ width: 300 }}>
-              <Slider min={0} max={40}
-                getAriaLabel={() => 'cbdLevel'}
+              <Slider
+                min={0}
+                max={40}
+                getAriaLabel={() => "thcLevel"}
                 defaultValue={[0, 40]}
                 getAriaValueText={valuetext}
                 step={5}
                 valueLabelDisplay="auto"
                 marks={marks}
-                onChange={(event, value) => filterElements('cbdLevel', value)}
+                onChange={(event, value) => filterElements("thcLevel", value)}
+              />
+            </Box>
+          </ListItem>
+        </List>
+
+        <List className="cbd level">
+          <ListItem>
+            <Box sx={{ width: 300 }}>
+              <Slider
+                min={0}
+                max={40}
+                getAriaLabel={() => "cbdLevel"}
+                defaultValue={[0, 40]}
+                getAriaValueText={valuetext}
+                step={5}
+                valueLabelDisplay="auto"
+                marks={marks}
+                onChange={(event, value) => filterElements("cbdLevel", value)}
               />
             </Box>
           </ListItem>
         </List>
       </Drawer>
-
     </Box>
   );
 };
