8a7a596b0065f53c6de070590469e5f8efe49f0e
Maciek <matomaciek@gmail.com>
2024-08-27 19:20:03 +0200

    adding filter for terpen

--- src/notatki/aplikacja-plan.ts
+++ src/notatki/aplikacja-plan.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export /*
 1. Dodac filtr dla terpenow => zostaly style do ogarniecia DONE
 2. Zrobic ProductPreview (obsluzyc dane, ktore pobieramy i wyswietlic je w przystepny dla uzytkownika sposob)
@@ -10,40 +9,19 @@ export /*
     - ma to jakos wygladac
     - w pasku gornym strony, lub w jakims menu powinien byc jakis odnosnik z przekierowaniem do register
     - Zrobic szyfrowanie hasla na backendzie*
-    - napisac do tego test jednostkowy*
-=======
-/*
-1. Dodac filtr dla terpenow
-2. Zrobic ProductPreview (obsluzyc dane, ktore pobieramy i wyswietlic je w przystepny dla uzytkownika sposob)
-3. Zrobic rejestracje
-    - Zrobic nowa podstrone Register
-    - Zrobic formularz rejestracji (z walidacja: email, haslo, powtorz haslo)
-    - Zrobic nowy endpoint na backendzie do rejestracji ( z zapisem uzytkownika do bazy)
-    - Spiac front i backend
-    - Zrobic szyfrowanie hasla na backendzie*
->>>>>>> d573f92ff1693f42afbd4cfa61c40af7a985fe65
+    - napisac do 
+
 4. Zrobic logowanie
     - Zrobic nowa podstrone Login
     - Zrobic formularz logowania (z walidacja)
     - Zrobic nowy endpoint na backendzie do logowania (sprawdzanie credentials)
-<<<<<<< HEAD
     - Zrobic autentykacje JWT* (json web token - poczytac, "how to use jwt with node and react")
 5. Zrobic ProtectedRoutes, czyli podstrony tylko dla zalogowanych uzytkownikow
 */
 /* 
-=======
-    - Zrobic autentykacje JWT*
-5. Zrobic ProtectedRoutes, czyli podstrony tylko dla zalogowanych uzytkownikow
-*/
 
-/*
->>>>>>> d573f92ff1693f42afbd4cfa61c40af7a985fe65
 Plan dzialania:
 1. Na kazde zadanie tworzymy oddzielny branch gita (git checkout -b <moja_nazwa_brancha> np: create-register)
 2. Po zrobieniu kazdego zadania robimy pull request
 3. Jak mamy approve to robimy merge
-<<<<<<< HEAD
 */ {};
-=======
-*/
->>>>>>> d573f92ff1693f42afbd4cfa61c40af7a985fe65
--- src/utils/switchTerpenIcon.tsx
+++ src/utils/switchTerpenIcon.tsx
@@ -1,24 +1,25 @@
-import SpaIcon from '@mui/icons-material/Spa';
-import { TerpenEnum } from '../types/Terpen';
+import SpaIcon from "@mui/icons-material/Spa";
+import { TerpenEnum } from "../types/Terpen";
+import { Box, styled } from "@mui/material";
+
+const StyledBox = styled(Box)({
+  background: "red",
+});
 
 export const switchTerpenIcon = (terpenType: TerpenEnum) => {
   // eslint-disable-next-line default-case
   switch (terpenType) {
-  case TerpenEnum.LIMONEN:
-    return (
-      <SpaIcon sx={{ color: 'yellow' }}/>
-    );
+    case TerpenEnum.LIMONEN:
+      return (
+        <StyledBox>
+          <SpaIcon sx={{ color: "yellow" }} /> {TerpenEnum.LIMONEN}
+        </StyledBox>
+      );
 
-  case TerpenEnum.CARIOPHILEN:
-    return (
-      <SpaIcon sx={{ color: 'green' }}/>
+    case TerpenEnum.CARIOPHILEN:
+      return <SpaIcon sx={{ color: "green" }} />;
 
-    );
-
-  case TerpenEnum.MIRCEN:
-    return (
-      <SpaIcon sx={{ color: 'purple' }}/>
-
-    );
+    case TerpenEnum.MIRCEN:
+      return <SpaIcon sx={{ color: "purple" }} />;
   }
 };
